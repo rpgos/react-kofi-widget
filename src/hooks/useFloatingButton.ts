38e91f435d4ff649ab046ead0a70cce8da62cdce
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-18 13:23:50 +0000

    Fix iFrame background for React apps

--- src/hooks/useFloatingButton.ts
+++ src/hooks/useFloatingButton.ts
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { KofiFloatingButtonProps } from "../components/KofiFloatingButton/KofiFloatingButton";
 
 export function useFloatingButton({ username, background, textColor, text }: KofiFloatingButtonProps) {
+  const intervalId = useRef<number | undefined>(undefined)
+  const retries = useRef<number>(0)
+
   useEffect(() => {
     if(document.querySelector(`script[src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"]`)) return;
 
@@ -11,7 +14,20 @@ export function useFloatingButton({ username, background, textColor, text }: Kof
     script.onload = draw
 
     document.body.appendChild(script)
+    intervalId.current = fixIframe()
   }, [])
+
+  const fixIframe = () => {
+    return setInterval(() => {
+      document.getElementsByClassName('floatingchat-container')[0].setAttribute('style', 'color-scheme: normal;')
+
+      if(retries.current >= 5) {
+        clearInterval(intervalId.current)
+      }
+
+      retries.current = retries.current + 1
+    }, 100)
+  }
 
   const draw = () => {
     if(typeof window.kofiWidgetOverlay !== 'undefined') {
